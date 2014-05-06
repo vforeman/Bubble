d267d51ec1151de55b7a724c44e0ad1ca23c91f1
V <V@As-iMac-3.local>
2014-05-06 16:36:11 -0400

    na

--- src/com/example/boilerapp/ShareActivity.java
+++ src/com/example/boilerapp/ShareActivity.java
@@ -1,5 +1,42 @@
 package com.example.boilerapp;
 
-public class ShareActivity {
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class ShareActivity extends Activity{
+	
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.share_activity);
+		Context ctx = getBaseContext();
+		Intent shareIntent = getIntent();
+		Uri uri = shareIntent.getData();		
+		WebView web = new WebView(ctx);
+		
+		
+//		if(!isNetworkAvailable()){
+//			web.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+//		}
+		
+		
+		
+		
+		
+	}
+	
+	public boolean isNetworkAvailable(){
+		ConnectivityManager connManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+		NetworkInfo activeNetworkInfo = connManager.getActiveNetworkInfo();
+		return activeNetworkInfo != null;
+	}
+	
+	
 
 }
