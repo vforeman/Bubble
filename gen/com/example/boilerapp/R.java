03ffe3461ca9575222fee272f8fe19512cd4eb98
errolgrannum <errolgrannum@gmail.com>
2014-05-06 19:25:08 -0400

    set up webview

--- gen/com/example/boilerapp/R.java
+++ gen/com/example/boilerapp/R.java
@@ -30,9 +30,9 @@ public final class R {
     public static final class id {
         public static final int LinearLayout2=0x7f090000;
         public static final int ListView1=0x7f090008;
-        public static final int RelativeLayout1=0x7f090009;
-        public static final int action_forgot_password=0x7f09000a;
-        public static final int action_settings=0x7f09000b;
+        public static final int RelativeLayout1=0x7f09000a;
+        public static final int action_forgot_password=0x7f09000b;
+        public static final int action_settings=0x7f09000c;
         public static final int email=0x7f090003;
         public static final int imageView1=0x7f090001;
         public static final int login=0x7f090004;
@@ -40,13 +40,15 @@ public final class R {
         public static final int new_account_button=0x7f090005;
         public static final int password=0x7f090002;
         public static final int textView=0x7f090006;
+        public static final int webview=0x7f090009;
     }
     public static final class layout {
         public static final int activity_login_signup=0x7f030000;
         public static final int home_activity=0x7f030001;
         public static final int share_activity=0x7f030002;
         public static final int share_template=0x7f030003;
-        public static final int splash_activity=0x7f030004;
+        public static final int share_webview=0x7f030004;
+        public static final int splash_activity=0x7f030005;
     }
     public static final class menu {
         public static final int login=0x7f080000;
--- src/com/example/boilerapp/ShareActivity.java
+++ src/com/example/boilerapp/ShareActivity.java
@@ -1,17 +1,16 @@
 package com.example.boilerapp;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.URL;
-
 import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.webkit.WebView;
 import android.widget.TextView;
 
 public class ShareActivity extends Activity {
+	
+	private WebView shareWebView;
+	
 	
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
@@ -20,44 +19,44 @@ public class ShareActivity extends Activity {
 		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
 		StrictMode.setThreadPolicy(policy);
 		
-		setContentView(R.layout.share_activity);
+		
+		//====Setting up Webview====
+		WebView web = new WebView(this);
+		setContentView(web);
+		web.getSettings().setJavaScriptEnabled(true);
+		
+		
+		//===Getting Information from Intent Filter ===
 		Intent intent = getIntent();
-		
 		//convert intent to string uri representation
-		String uriString = intent.toUri(0);
-				
-		//Parsing the uri string
-		
-		Uri uri = Uri.parse(uriString);
-			
+		String uriString = intent.toUri(0);				
 		TextView text = (TextView) findViewById(R.id.textView);
-		//Getting the action of the intent
 		String action = intent.getAction();
 		
-		//test if uri shows up
-		text.append(uriString);
-		text.append("===break===");
+		
+		 
+		if (action.equalsIgnoreCase(Intent.ACTION_SEND) && intent.hasExtra(Intent.EXTRA_TEXT)) { 
+		    String url = intent.getStringExtra(Intent.EXTRA_TEXT); 
+		    web.loadUrl(url);
+		} 
 		
 		
-		if (!action.equals(Intent.ACTION_SEND)){
-			throw new RuntimeException("Error Wrong Action");
-		}
-		//To Get Data
-		Uri data = intent.getData();
-		URL url;
-		try {
-			url = new URL(data.getScheme(), data.getHost(), data.getPath());
-			BufferedReader rd = new BufferedReader(new InputStreamReader(url.openStream()));
-			String line = "";
-			while((line = rd.readLine()) != null){
-				text.append(line);
-			}
-			
-		} catch (Exception e){
-			e.printStackTrace();
-		}
-		
-	}
+				
 	
-
+		
+//		URL url;
+//		try {
+//			url = new URL(data.getScheme(), data.getHost(), data.getPath());
+//			BufferedReader rd = new BufferedReader(new InputStreamReader(url.openStream()));
+//			String line = "";
+//			while((line = rd.readLine()) != null){
+//				text.append(line);
+//			}
+//			
+//		} catch (Exception e){
+//			e.printStackTrace();
+//		}
+	}
 }
+	
+	
